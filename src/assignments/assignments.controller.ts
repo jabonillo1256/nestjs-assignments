ffaa7bef57e0136f90e4f015fe0719b15909e653
Vio-lette <137487282+jabonillo1256@users.noreply.github.com>
2024-11-08 16:01:37 +0800

    assignment 3 factorial completed all working

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';  
 
 @Controller('assignments')
-export class AssignmentsController {}
+
+export class AssignmentsController {  
+  @Get('factorial/:number')  
+  getFactorial(@Param('number') number: string): { factorial: number } {  
+    const num = parseInt(number, 10);  
+    
+    if (isNaN(num) || num < 0) {  
+      throw new NotFoundException('Enter a non-negative number');  
+    }  
+
+    const factorial = this.calculateFactorial(num);  
+    return { factorial };  
+  }  
+
+  private calculateFactorial(num: number): number {  
+    if (num === 0 || num === 1) return 1;  
+    return num * this.calculateFactorial(num - 1);  
+  }  
+
+}
