f8f1076c260d150911f4feccfd59c37b401bd2fe
Vio-lette <137487282+jabonillo1256@users.noreply.github.com>
2024-11-08 15:45:24 +0800

    assignment 2 prime number is done

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentsController {
+  @Get('prime/:number')
+  determinePrimeStatus(@Param('number', ParseIntPipe) number: number): { isPrime: boolean } {
+    return { isPrime: this.isNumberPrime(number) };
+  }
+
+  private isNumberPrime(num: number): boolean {
+    if (num < 2) return false;
+    for (let divisor = 2; divisor <= Math.sqrt(num); divisor++) {
+      if (num % divisor === 0) return false;
+    }
+    return true;
+  }
+}
